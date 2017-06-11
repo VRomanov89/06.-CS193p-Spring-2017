d62916261e362b377df08f1e65d21924514fc2c6
Vladimir Romanov <vladromweb@gmail.com>
2017-06-10 21:30:53 -0700

    finished keypad
    finished keypad

--- 01. Calculator/CalculatorApp/CalculatorApp/ViewController.swift
+++ 01. Calculator/CalculatorApp/CalculatorApp/ViewController.swift
@@ -9,17 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        let textCurrentlyInDisplay = display!.text!
+        display!.text = textCurrentlyInDisplay + digit
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    @IBOutlet weak var display: UILabel?
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    
+    
+    
+    
+    
+    
 }
 
