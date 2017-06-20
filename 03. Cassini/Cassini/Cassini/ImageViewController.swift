1a98c7bf00bbdd55ea2bb32eb433a09b9f4b604d
Vladimir Romanov <vladromweb@gmail.com>
2017-06-19 22:33:49 -0700

    lecture 7 finished cassini
    lecture 7 finished cassini

--- 03. Cassini/Cassini/Cassini/ImageViewController.swift
+++ 03. Cassini/Cassini/Cassini/ImageViewController.swift
@@ -38,10 +38,14 @@ class ImageViewController: UIViewController {
     }
     @IBOutlet weak var scrollView: UIScrollView! {
         didSet {
+            scrollView.delegate = self
+            scrollView.minimumZoomScale = 0.03
+            scrollView.maximumZoomScale = 1.0
+            scrollView.contentSize = imageView.frame.size
             scrollView.addSubview(imageView)
         }
     }
-    private var imageView = UIImageView()
+    fileprivate var imageView = UIImageView()
     private var image: UIImage? {
         get {
             return imageView.image
@@ -49,6 +53,13 @@ class ImageViewController: UIViewController {
         set {
             imageView.image = newValue
             imageView.sizeToFit()
+            scrollView?.contentSize = imageView.frame.size
         }
+    }
+}
+
+extension ImageViewController : UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
     }
 }
