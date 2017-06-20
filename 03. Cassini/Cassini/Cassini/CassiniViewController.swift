615ae8c5472c10b0ace49d325552188b28b5e061
Vladimir Romanov <vladromweb@gmail.com>
2017-06-20 12:00:06 -0700

    cassini in split views
    cassini in split views

--- 03. Cassini/Cassini/Cassini/CassiniViewController.swift
+++ 03. Cassini/Cassini/Cassini/CassiniViewController.swift
@@ -9,8 +9,25 @@
 import UIKit
 
 class CassiniViewController: UIViewController {
-
+    
+    // MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let url = DemoURL.NASA[segue.identifier ?? ""] {
+            if let imageVC = (segue.destination.contents as? ImageViewController) {
+                imageVC.imageURL = url
+                imageVC.title = (sender as? UIButton)?.currentTitle
+            }
+        }
     }
 
+}
+
+extension UIViewController {
+    var contents: UIViewController {
+        if let navcon = self as? UINavigationController {
+            return navcon.visibleViewController ?? self
+        } else {
+            return self
+        }
+    }
 }
